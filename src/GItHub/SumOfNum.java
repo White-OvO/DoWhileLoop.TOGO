871516dc471f1a664751b159097a66c561a15107
Christopher G Gonzalez <christopherggonzalez@Christophers-MacBook-Air.local>
2024-01-05 22:26:18 -0800

    active counter to stop the program

--- src/GItHub/SumOfNum.java
+++ src/GItHub/SumOfNum.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class SumOfNum {
 	public static void main(String[] args) { 
 		// declaration 
-		//int Counter; 
+		int Counter;  // to stop the program from running
 		
 		// loop
 		do { 
@@ -14,16 +14,19 @@ public class SumOfNum {
 	         Scanner in = new Scanner(System.in);
 	            System.out.println("Enter  3 numbers to be added: ");
 			for (double i = 0; i < 3; i++) {   // loop until i reaches 3
-				//list[i] = in.nextDouble();
+				//list[i] = in.nextDouble();			// wrong 
 			      list[(int)i] = in.nextDouble();
-			//sum += list[i];
+			//sum += list[i];    // wrong
 			      sum += list[(int)i];
 		} 
 			System.out.println("sum = " + sum + "\n");
-			
-			
+			 
+			 System.out.println("Enter 1 to continue or any value to exit........");
+	            Counter = in.nextInt();
 		}
-		while (true);   // this is need to close the do while looop 
+		//while (true);   // this is need to close the do while looop 
+			while (Counter == 1); // counter to stop the program
+			System.out.println("Thank you have a wounderful day");
 	}
 	
 
